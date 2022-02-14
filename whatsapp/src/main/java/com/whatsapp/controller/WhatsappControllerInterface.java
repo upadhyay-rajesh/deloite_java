9e10066d64a15f7ee3b901f32ebe55f34423d4d0
Rajesh Upadhyay <rajesh.upadhyay@publicissapient.com>
2022-02-14 10:23:48 +0530

    zxcz

---
+++ whatsapp/src/main/java/com/whatsapp/controller/WhatsappControllerInterface.java
@@ -0,0 +1,13 @@
+package com.whatsapp.controller;
+
+public interface WhatsappControllerInterface {
+
+	void createProfileController();
+
+	void viewallProfileController();
+
+	void viewProfileController();
+
+	void deleteProfileController();
+
+}
